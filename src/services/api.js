9492b2a1843fa7afcd8192286b7353d54dc38c00
Era-smus <erasmussaat1@gmail.com>
2025-12-15 16:01:37 +0300

    Modified api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://json-server-vercel-rqie.vercel.app';
+const API_BASE_URL = 'https://json-server-vercel-rqie.vercel.app/activities';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
